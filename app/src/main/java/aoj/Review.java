57727b93f594352b6923221a17941e81c5d0b92c
Akira Toyoda <yoooh723.west24@gmail.com>
2022-07-19 07:54:17 +0900

    review matrix-chain multiplication

--- app/src/main/java/aoj/Review.java
+++ app/src/main/java/aoj/Review.java
@@ -1,75 +1,32 @@
 package aoj;
 
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Review {
-    static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
-
-    static void maxHeapify(int[] heap, int n, int cur) {
-        int largest = cur;
-        int left = 2 * cur;
-        if (left <= n && heap[left] > heap[largest]) {
-            largest = left;
-        }
-        int right = 2 * cur + 1;
-        if (right <= n && heap[right] > heap[largest]) {
-            largest = right;
-        }
-
-        if (largest != cur) {
-            swap(heap, largest, cur);
-            maxHeapify(heap, n, largest);
-        }
-    }
-
-    static void heapSort(int[] heap) {
-        int n = heap.length - 1;
-        for (int i = n / 2; i >= 1; i--) {
-            maxHeapify(heap, n, i);
-        }
-
-        while (n >= 1) {
-            swap(heap, 1, n);
-            n--;
-            maxHeapify(heap, n, 1);
-        }
-    }
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int[] heap = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            heap[i] = s.nextInt();
+        int[] rowCols = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            rowCols[i] = s.nextInt();
+            rowCols[i + 1] = s.nextInt();
         }
 
-        heapSort(heap);
-
-        int heapSize = 1;
-        while (heapSize < n) {
-            int cur = heapSize;
-            while (cur >= 2) {
-                swap(heap, cur, cur / 2);
-                cur = cur / 2;
+        int[][] dp = new int[n + 1][n + 1];
+        for (int num = 2; num <= n; num++) {
+            for (int first = 1; first <= n - num + 1; first++) {
+                int last = first + num - 1;
+                dp[first][last] = Integer.MAX_VALUE;
+                for (int mid = first; mid <= last - 1; mid++) {
+                    dp[first][last] = Math.min(
+                        dp[first][last],
+                        dp[first][mid] + dp[mid + 1][last] + rowCols[first - 1] * rowCols[mid] * rowCols[last]
+                    );
+                }
             }
-
-            heapSize++;
-            swap(heap, 1, heapSize);
         }
 
-        System.out.println(
-            Arrays.stream(heap).boxed()
-                .skip(1)
-                .map(String::valueOf)
-                .collect(Collectors.joining(" "))
-        );
-
+        System.out.println(dp[1][n]);
         s.close();
     }
 }
